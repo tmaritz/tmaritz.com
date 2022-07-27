3ccbdfce322294264d4134a574898211285ab30f
tmaritz <33519371+tmaritz@users.noreply.github.com>
2022-07-27 08:45:41 +0100

    remove commented code, change pic format

--- src/Components/About.js
+++ src/Components/About.js
@@ -5,22 +5,16 @@ class About extends Component {
 
     if(this.props.data){
       var name = this.props.data.name;
-      var profilepic= "images/"+this.props.data.image;
+      var profile_pic= "images/"+this.props.data.image;
       var bio = this.props.data.bio;
-      var street = this.props.data.address.street;
-      var city = this.props.data.address.city;
-      var state = this.props.data.address.state;
-      var zip = this.props.data.address.zip;
-      var phone= this.props.data.phone;
       var email = this.props.data.email;
-      var resumeDownload = this.props.data.resumedownload;
     }
 
     return (
       <section id="about">
       <div className="row">
          <div className="three columns">
-            <img className="profile-pic"  src={profilepic} alt="Toby Wolff-Maritz Profile Pic" />
+            <img className="profile-pic"  src={profile_pic} alt="Toby Wolff-Maritz Profile Pic" />
          </div>
          <div className="nine columns main-col">
             <h2>About Me</h2>
@@ -31,18 +25,9 @@ class About extends Component {
                   <h2>Contact Details</h2>
                   <p className="address">
 						   <span>{name}</span><br />
-						   {/* <span>{street}<br />
-						         {city} {state}, {zip}
-                   </span><br /> */}
-						   {/* <span>{phone}</span><br /> */}
                      <span>{email}</span>
 					   </p>
                </div>
-               {/* <div className="columns download">
-                  <p>
-                     <a href={resumeDownload} className="button"><i className="fa fa-download"></i>Download Resume</a>
-                  </p>
-               </div> */}
             </div>
          </div>
       </div>
